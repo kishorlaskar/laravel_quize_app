0c19134a1e4d35dec975b08c929439de3f3cea59
Kishor Laskar <30683532+kishorlaskar@users.noreply.github.com>
2020-10-22 22:38:25 +0600

    Update MainController.php

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,8 +6,56 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    public  function  index()
-    {
-        return view('Home.home');
-    }
+        public  function  startquize()
+        {
+               Session::put('nextq',1);
+               Session::put('wrongans',0);
+               Session::put('correctans',0);
+
+               $q= Question::all()->first();
+               return view('answer')->with(['question'=>$q]);
+
+        }
+        public function submitans(Request $request)
+        {
+            $nextq = Session::get('nextq');
+            $wrongans = Session::get('wrongans');
+            $correctans = Session::get('correctans');
+            $request->validate([
+              'question'=> 'required',
+              'dbans' => 'required',
+          ]);
+
+
+          $nextq+=1;
+          if ($request->dbans == $request->ans)
+          {
+              $correctans+=1;
+          }
+          else
+              {
+                  $wrongans+=1;
+              }
+          Session::put("nextq",$nextq);
+          Session::put("wrongans",$wrongans);
+          Session::put("correctans",$correctans);
+
+           $i=0;
+
+           $question = Question::all();
+
+           foreach ($question as $question)
+           {
+               $i++;
+               if($question->count() < $nextq)
+               {
+                   return view('end');
+               }
+               if ($i==$nextq)
+               {
+                   return view('answer')->with(['question'=>$question]);
+               }
+           }
+
+        }
 }
